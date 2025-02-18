25b54b7fbd213f835d4f14a915ebe2130e282520
LimChangeun <cksrms1030@naver.com>
2025-02-18 19:14:56 +0900

    [Feat] report add

---
+++ src/main/java/com/example/breadbook/Report/ReportService.java
@@ -0,0 +1,34 @@
+package com.example.breadbook.Report;
+
+import com.example.breadbook.Report.model.Report;
+import com.example.breadbook.Report.model.ReportDto;
+import com.example.breadbook.domain.member.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+    private final ReportRepository reportRepository;
+
+    public void register(ReportDto.ReportRegister dto, Member member, Product product) {
+        Report report = reportRepository.save(dto.toEntity(product, member));
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReportDto.ReportResponse>list() {
+        List<Report> reportList = reportRepository.findAll();
+
+        return reportList.stream().map(ReportDto.ReportResponse::from).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ReportDto.ReportResponse read(Long reportIdx) {
+        Report report = reportRepository.findById(reportIdx).orElseThrow();
+        return ReportDto.ReportResponse.from(report);
+    }
+}
---
+++ src/main/java/com/example/breadbook/Report/model/Report.java
@@ -0,0 +1,33 @@
+package com.example.breadbook.Report.model;
+
+import com.example.breadbook.domain.member.model.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private Long member_idx;
+    private Long product_idx;
+    private String report_reason;
+    private Date create_at;
+
+    @OneToOne
+    @JoinColumn(name = "member_idx")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "product_idx")
+    private Product product;
+}
