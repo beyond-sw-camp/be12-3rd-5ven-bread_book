100c206c1a9ae79682cbee457dbf06a69c61da04
choi-won-ik <whwwhs7837@gmail.com>
2025-02-21 16:51:04 +0900

    [Fix] 구매정보 & 판매정보 수정

--- src/main/java/com/example/breadbook/global/response/BaseResponse.java
+++ src/main/java/com/example/breadbook/global/response/BaseResponse.java
@@ -1,16 +1,26 @@
 package com.example.breadbook.global.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class BaseResponse<T> {
     private Boolean isSuccess;
     private String message;
     private Integer code;
     private T data;
+
+    public BaseResponse(BaseResponseMessage baseResponseMessage, T data){
+        this.isSuccess = baseResponseMessage.getIsSuccess();
+        this.message = baseResponseMessage.getMessage();
+        this.code = baseResponseMessage.getCode();
+        this.data = data;
+    }
+
+    public BaseResponse(BaseResponseMessage baseResponseMessage){
+        this.isSuccess = baseResponseMessage.getIsSuccess();
+        this.message = baseResponseMessage.getMessage();
+        this.code = baseResponseMessage.getCode();
+    }
 }
+
