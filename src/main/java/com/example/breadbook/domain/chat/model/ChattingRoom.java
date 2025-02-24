ff3bfea80e5c98b843d1b65fe72a97e469482459
wkdlrn <kkjjgg97@gmail.com>
2025-02-24 16:18:47 +0900

    Feat: 채팅방 엔티티 추가 #45

---
+++ src/main/java/com/example/breadbook/domain/chat/model/ChattingRoom.java
@@ -0,0 +1,35 @@
+package com.example.breadbook.domain.chat.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChattingRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column(nullable = false, unique = true)
+    private String identifier;
+
+    @Column(name = "product_idx")
+    private Long productIdx;
+
+    @Column(columnDefinition = "TEXT")
+    private String lastChat;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "room")
+    private List<Message> messages = new ArrayList<>();
+}
