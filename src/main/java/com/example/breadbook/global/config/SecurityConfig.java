ecab90e13ef2f0c1ba6b819e7bc08189205a122a
choi-won-ik <whwwhs7837@gmail.com>
2025-02-25 17:29:15 +0900

    [Fix] login url변경

--- src/main/java/com/example/breadbook/global/config/SecurityConfig.java
+++ src/main/java/com/example/breadbook/global/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(
                 (auth) -> auth
-                        .requestMatchers("/user/signup", "/login","/logout",
+                        .requestMatchers("/user/signup", "/user/login","/logout",
                                 "/user/signup-oauth", "/user/auth/check",
                                 "/error", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/swagger-resources/**","cookies/user").permitAll()
