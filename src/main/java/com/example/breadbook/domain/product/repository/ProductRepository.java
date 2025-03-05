caecfa5d6870c8fb68251a51dc6e059eadc093a3
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-03-05 12:05:53 +0900

    [Feat] ProductController에 BaseResponse 적용, readItem 기능 추가

--- src/main/java/com/example/breadbook/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/breadbook/domain/product/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.example.breadbook.domain.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public Product findByIdx(Long idx);
 }
