c1638283fb26d7b3a5a73fd66f52e0b0cdd25cd5
InukChoi <iu.choi3197@gmail.com>
2025-03-07 16:27:33 +0900

    [Feat] member 관련 예외 처리

--- src/main/java/com/example/breadbook/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/breadbook/global/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.breadbook.global.handler;
 
+import com.example.breadbook.global.response.BaseResponse;
+import com.example.breadbook.global.response.BaseResponseMessage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +16,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> IOException(IOException e) {
         e.printStackTrace();
         return ResponseEntity.badRequest().body("에러");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // 어떤 변수에 값을 설정하다가 에러가 났는지
+        System.out.println(e.getBindingResult().getFieldError().getField());
+
+        // 입력값 검증 할 때 작성한 message를 받아오는 코드
+        System.out.println(e.getBindingResult().getFieldError().getDefaultMessage());
+
+        return ResponseEntity.badRequest().body(new BaseResponse<String>(BaseResponseMessage.REQUEST_FAIL,
+                e.getBindingResult().getFieldError().getDefaultMessage()));
     }
 }
